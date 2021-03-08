40e797763cc10f8a955d8462ae4e6dbefb203eb2
Marden Rocha <mardenbiel@gmail.com>
2021-03-08 20:17:20 -0300

    Remove eureka discovery.

--- src/main/java/br/com/testeatlantico/api/ApiApplication.java
+++ src/main/java/br/com/testeatlantico/api/ApiApplication.java
@@ -1,16 +1,11 @@
 package br.com.testeatlantico.api;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableDiscoveryClient
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
